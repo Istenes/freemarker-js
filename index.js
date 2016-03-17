ca71a08b12b8ed9d295525b20dbcdf7df50540d5
Niklas Istenes <niklas.istenes@teliasonera.com>
2016-03-17 14:47:47 +0100

    added Q chains to generate multiple freemarkerfiles

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var path = require("path"),
     jarFile = path.join(__dirname, "FMRender-0.7.jar"),
     iconv = require("iconv-lite"),
-    spawn = require('child_process').spawn;
+    spawn = require('child_process').spawn
+    q = require('q');
 
 var freemarker = freemarker || {};
-freemarker.process = function(args) {
+freemarker.process = function(args, appendData) {
+    var deferred = q.defer();
     var dataModel = JSON.stringify(args.data);
     var settingsString = JSON.stringify(args.settings);
     var deps = JSON.stringify(args.deps);
     var nodes = JSON.stringify(args.nodes);
     var settings = args.settings;
     var resultData = "";
+
+    var hasErrors = false;
 
     var cmd = spawn('java', ["-jar", jarFile,
         settingsString,
@@ -20,23 +24,36 @@ freemarker.process = function(args) {
         nodes
     ]);
 
-    if (args.callback) {
         cmd.stdout.on("data", function(data) {
-            // args.callback(null, iconv.decode(data, settings.encoding));
             resultData += iconv.decode(data, settings.encoding);
         });
         cmd.stderr.on("data", function(data) {
             // Print error message
+            hasErrors = true;
             var result = '<div align="left" style="background-color:#FFFF00; color:#FF0000; display:block; border-top:double; padding:2pt; font-size:medium; font-family:Arial,sans-serif; font-style: normal; font-variant: normal; font-weight: normal; text-decoration: none; text-transform: none">'
             result += iconv.decode(data, settings.encoding).replace(/[\n\r]/g, "<br/>");
             result += '</div>'
             resultData += result;
-            console.log(iconv.decode(data, settings.encoding));
         });
         cmd.stdout.on("end", function() {
-            args.callback(null, resultData);
+            if (args.callback) {
+                if(appendData) {
+                    appendData += resultData;
+                    args.callback(null, appendData);
+                } else {
+                    args.callback(null, resultData);
+                }
+            } else {
+                if(appendData) {
+                    appendData += resultData;
+                    deferred.resolve(appendData);
+                } else {
+                    deferred.resolve(resultData);
+                }
+            }
         });
-    }
+
+        return deferred.promise;
 }
 
 module.exports = freemarker;
